5db64dc310a1a89140963d3b4fc28efd8a10a1c2
klu_2200031618 <kk258786@gmail.com>
2024-06-20 22:14:33 +0530

    ALl the required files that are needed are added, need to complete nav bar tomorrow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/home'
 function App() {
   return (
     <>
-      HI
+      <Routes>
+        <Route path="/"  element={<Home />}/>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+export const Home=()=>{
+    return(
+        <div>
+                Hello from home
+        </div>
+    )
+}
