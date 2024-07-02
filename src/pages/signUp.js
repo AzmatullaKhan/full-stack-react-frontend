3b6d607684437075851d8c4528dfd798dd2e6b16
klu_2200031618 <kk258786@gmail.com>
2024-07-02 17:24:42 +0530

    Connection between frontend-backend(SignIn) has been set, added all alerts, expected logic has been perfectly done, need to complete login or userprofile

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,7 +1,10 @@
+// import { useState } from 'react';
 import './allPages.css'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export const SignUp=()=>{
+
     
     let navigate= useNavigate();
     const handleHomeClick=()=>{
@@ -35,10 +38,10 @@ export const SignUp=()=>{
     }
 
     const validateNumber=()=>{
-        let signgup_number=document.getElementById('signgup_number');
-        let last_char=signgup_number.value[signgup_number.value.length-1];
+        let signup_number=document.getElementById('signup_number');
+        let last_char=signup_number.value[signup_number.value.length-1];
         if(!(last_char>='0' && last_char<='9')){
-            signgup_number.value=signgup_number.value.slice(0, signgup_number.value.length-1);
+            signup_number.value=signup_number.value.slice(0, signup_number.value.length-1);
             document.getElementById('number_label').style.animation='error_animation-animation 1s ease-in-out 0s'
             document.getElementById('signup_home').style.pointerEvents='none'
             setTimeout(()=>{
@@ -46,8 +49,8 @@ export const SignUp=()=>{
                 document.getElementById('signup_home').style.pointerEvents=''
             },2000)
         }
-        else if(signgup_number.value.length>3){
-            let v=signgup_number.value
+        else if(signup_number.value.length>3){
+            let v=signup_number.value
             document.getElementById('signup_container_three_numberLast').textContent=v.slice(v.length-3, v.length)
         }
     } 
@@ -56,7 +59,6 @@ export const SignUp=()=>{
         let char=e.target.value;
         char=char[char.length-1]
         if(!((char>='a' && char<='z') ||(char>='A' && char<='Z')|| (char>='0' && char<='9') || char==='@' || char==='$' || char==='_')){
-            console.log(e.target.value=e.target.value.slice(0, e.target.value.length-1))
             document.getElementById('username_label').style.animation='error_animation-animation 1s ease-in-out 0s'
             setTimeout(()=>{
                 document.getElementById('username_label').style.animation=''
@@ -64,15 +66,20 @@ export const SignUp=()=>{
         }
     }
 
+    function getGender(){
+        if(document.getElementById('signup_genderMale').isChecked)
+            return 'Male'
+        else if(document.getElementById('signup_genderFemale').isChecked)
+            return 'Female'
+        else
+            return 'Others'
+    }
+
+
+
     const validate=(e)=>{
         e.preventDefault();
         let pass=validatePassword(document.getElementById('signup_password').value)
-
-        if(pass){
-            document.getElementById('signup_container_three').className='signup_container_three_visible'
-            document.getElementById('signup_container_two').className='signup_container_two_hidden'
-            document.getElementById('signup_container_one_image_signup').className='signup_container_two_hidden'
-        }
 
         if(!pass){
             document.getElementById('password_label').style.animation='error_animation-animation 1s ease-in-out 0s'
@@ -82,11 +89,77 @@ export const SignUp=()=>{
                 document.getElementById('signup_home').style.pointerEvents=''
             },2000)
         }
-            
-    }
 
+        if(pass){
+            let username =document.getElementById('signup_username').value
+            let mobileNumber =document.getElementById('signup_number').value
+            let pass1;
+            let pass2;
+
+            axios.post('http://localhost:5000/singUpServer/userExist',{ 
+                username
+            })
+            .then(res=>{(localStorage.setItem('respponseUsername', res.data.message))})
+            .catch((err)=>{console.log(err.message+"1")})
+
+            
+            axios.post('http://localhost:5000/singUpServer/numberExist',{ 
+                mobileNumber
+            })
+            .then(res=>{localStorage.setItem('responseMobileNumber',res.data.message)})
+            .catch((err)=>{console.log(err.message)})
+            
+            setTimeout(()=>{
+                pass1=(localStorage.getItem('respponseUsername'))
+                pass2=(localStorage.getItem('responseMobileNumber'))
+
+                if(pass1==='stop' && pass2==='stop')
+                    alert('A user with same mobile number and username already exist')
+                else if(pass1==='stop')
+                    alert('A user with same Username already exists')
+                else if(pass2==='stop')
+                    alert('A user with same Mobile Number already exist')
+                else{
+                    document.getElementById('signup_container_three').className='signup_container_three_visible'
+                    document.getElementById('signup_container_two').className='signup_container_two_hidden'
+                    document.getElementById('signup_container_one_image_signup').className='signup_container_two_hidden'
+                }
+            },100)
+
+
+        }
+    }
+    // axios.get('http://localhost:5000/singUpServer')
+    // .then(res=>{console.log(res.data.signUps)})
+    // .catch(err=>{console.log(err)})
+    
     const otpValidation=(e)=>{
         e.preventDefault()
+
+        let v1=document.getElementById('signup_container_three_input_1').value
+        let v2=document.getElementById('signup_container_three_input_2').value
+        let v3=document.getElementById('signup_container_three_input_3').value
+        let v4=document.getElementById('signup_container_three_input_4').value
+
+        if(v1==='1' && v2==='2' && v3==='3' && v4==='4'){
+            let username =document.getElementById('signup_username').value
+            let password =document.getElementById('signup_password').value
+            let mobileNumber =document.getElementById('signup_number').value
+            let dateOfBirth =document.getElementById('signup_date').value
+            let gender= getGender()
+            
+            axios.post('http://localhost:5000/singUpServer/addUser',{ 
+                username, password, mobileNumber, dateOfBirth, gender
+            })
+            .then(res=>{return res})
+            .catch((err)=>{console.log(err.message)})
+
+            navigate('/login')
+        }
+        else{
+            alert('Enter Correct OTP, hint:1,2,3,4')
+        }
+
     }
 
     const handleEyeClick=()=>{
@@ -99,7 +172,6 @@ export const SignUp=()=>{
             document.getElementById('signup_password').type='password'
             document.getElementById('signup_container_two_input_small_eye_cross').style.opacity=1;  
         }
-        console.log()
     }
 
     const handleOtpChange=(e)=>{
@@ -111,6 +183,7 @@ export const SignUp=()=>{
             e.target.value=''
         }
     }
+
     
     return(
         <div className="signup_container_one">
@@ -124,7 +197,7 @@ export const SignUp=()=>{
                     <h1>SignUp 📃</h1>
                     <div style={{display:'flex', flexDirection:'column', margin:"10px 0px", height:"70px"}}>
                         <div><label className='signup_container_two_label' id='username_label'>Username</label><span className='signup_container_two_label_errormessage' id='signup_container_two_label_one'>  *Use only (@,$,_) in your name</span></div>
-                        <input type="text" className='signup_container_two_input' placeholder='***** Enter name you want to register *****' required onChange={validateName}/>
+                        <input type="text" className='signup_container_two_input' placeholder='***** Enter name you want to register *****' id="signup_username" required onChange={validateName}/>
                     </div>
                     <div style={{display:'flex', flexDirection:'column', justifyContent:"flex-start",width:"100%", margin:"10px 0px", height:"70px"}}>
                         <label className='signup_container_two_label' id='password_label'>Password</label>
@@ -133,11 +206,11 @@ export const SignUp=()=>{
                     </div>
                     <div style={{display:'flex', flexDirection:'column', justifyContent:"flex-start",width:"100%",margin:"10px 0px", height:"70px"}}>
                         <div><label className='signup_container_two_label' id='number_label'>Number</label><span className='signup_container_two_label_errormessage'> *Only Numbers(0-9)</span></div>
-                        <input type="text" className='signup_container_two_input_small' placeholder='***** Enter your mobile number *****' required onChange={validateNumber} id='signgup_number' maxLength={10} minLength={10}/>
+                        <input type="text" className='signup_container_two_input_small' placeholder='***** Enter your mobile number *****' required onChange={validateNumber} id='signup_number' maxLength={10} minLength={10}/>
                     </div>
                     <div style={{display:'flex', flexDirection:'column', justifyContent:"flex-start",width:"100%",margin:"10px 0px", height:"70px"}}>
                         <label className='signup_container_two_label'>Date Of Birth</label>
-                        <input type="date" className='signup_container_two_input_small' max={getTodayDate()} required/>
+                        <input type="date" className='signup_container_two_input_small' max={getTodayDate()} required id="signup_date"/>
                     </div>
                     <div style={{display:'flex', flexDirection:'column',width:"100%",margin:"10px 0px", height:"70px"}}>
                         <div><label className='signup_container_two_label' id='gender_label'>Gender</label><span className='signup_container_two_label_errormessage' id='signup_container_two_label_two'>  *Must select gender</span></div>
@@ -162,10 +235,10 @@ export const SignUp=()=>{
                 <p>* Might take some while to receive a OTP</p>
                 <form onSubmit={otpValidation}>
                     <div style={{display:'flex'}}>
-                        <input type='text' className='signup_container_three_input' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
-                        <input type='text' className='signup_container_three_input' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
-                        <input type='text' className='signup_container_three_input' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
-                       <input type='text' className='signup_container_three_input' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
+                        <input type='text' className='signup_container_three_input' id='signup_container_three_input_1' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
+                        <input type='text' className='signup_container_three_input' id='signup_container_three_input_2' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
+                        <input type='text' className='signup_container_three_input' id='signup_container_three_input_3' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
+                        <input type='text' className='signup_container_three_input' id='signup_container_three_input_4' onChange={handleOtpChange} minLength={1} maxLength={1} required/>
                     </div>
                     <button className='signup_container_three_button' type='submit'>Submit</button>
                  </form>
