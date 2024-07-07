2d423c2050c01a8102858e72f424aea951b6e00f
klu_2200031618 <kk258786@gmail.com>
2024-07-07 22:28:46 +0530

    Added logic to change password from Profile, successfully completed

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,6 @@
 import './allPages.css'
 import { Navbar } from './nav'
+import axios from "axios"
 
 export const Profile=()=>{
     let username, mobileNumber, dateOfBirth, gender, password;
@@ -11,7 +12,6 @@ export const Profile=()=>{
 
     let isMale = (gender==='Male')?true:false;
 
-    console.log(isMale, gender)
     const handleEyeClick=()=>{
         let t=document.getElementById('profile_password').type
     if(t==='password'){
@@ -24,6 +24,61 @@ export const Profile=()=>{
     }
     }
 
+    const handlePasswordChange = (e)=>{
+        if(e.target.value===password)
+            document.getElementById('profile_container_three_button').className='profile_container_three_button_hidden'
+        else{
+            document.getElementById('profile_container_three_button').className='profile_container_three_button'
+        }
+    }
+
+    function validatePassword(pass){
+        let cap=Number(0);
+        let sp=Number(0);
+        let num=Number(0);
+        let len=pass.length>=8
+        for(let i=0;i<pass.length;i++){
+            if((pass[i]>='A' && pass[i]<='Z') && !cap)
+                cap=1;
+            if((pass[i]==='@' || pass[i]==='#' || pass[i]==='$' || pass[i]==='_' || pass[i]==='&') && !sp)
+                sp=1;
+            else if((pass[i]>='0' && pass[i]<='9') && !num)
+                num=1;
+        }
+        return (cap && sp && num && len)
+    }
+
+    const handleSaveChanges=(e)=>{
+        e.preventDefault()
+        let pass=validatePassword(document.getElementById('profile_password').value)
+
+        if(!pass){
+            document.getElementById('password_label').style.animation='error_animation-animation 1s ease-in-out 0s'
+            setTimeout(()=>{
+                document.getElementById('password_label').style.animation=''
+            },2000)
+        }
+
+        if(pass){
+            password=document.getElementById('profile_password').value
+            let mobileNumber= document.getElementById('profile_mobileNumber').value
+
+            // console.log(mobileNumber,pass)
+            axios.post('http://localhost:5000/loginServer/updaePassword',{
+                mobileNumber, password
+            })
+            .then(res=>{console.log(res)})
+            .catch(err=>{console.log(err)})
+
+            document.getElementById('profile_container_three_button').className='profile_container_three_button_hidden'
+
+            setTimeout(()=>{
+                localStorage.setItem('password', password)
+                console.log(localStorage.getItem('password'))
+            },100)
+        }
+    }
+
 return(
     <div className="profile_container_one">
         <Navbar />
@@ -33,23 +88,27 @@ return(
                 (<img className='profile_container_two_image' src={require('../images/profileMale.png')} alt='gender'/>):
                 (<img className='profile_container_two_image' src={require('../images/profileFemale.png')} alt='gender'/>)
             }
-            <div className="profile_container_three">
-                <label className='profile_container_three_label'>Username</label>
-                <input className='profile_container_three_input' type='text' readOnly value={username}/><br></br>
+            <form>
+                <div className="profile_container_three">
+                    <label className='profile_container_three_label'>Username</label>
+                    <input className='profile_container_three_input' type='text' readOnly value={username}/><br></br>
 
-                <label className='profile_container_three_label'>Password</label>
-                <input className='profile_container_three_input' type='password' readOnly value={password} id="profile_password"/>
-                <div className='profile_container_thre_input_eye' onClick={handleEyeClick}>👁️<p id='profile_container_three_input_eye_cross' style={{position:'relative', top:'-32px', left:'3px', fontSize:"24px", opacity:'1', marginTop:"7px"}}>/</p></div>
+                    <label className='profile_container_three_label' id='password_label'>Password</label>
+                    <input className='profile_container_three_input' type='password' defaultValue={password} id="profile_password" onChange={handlePasswordChange} minLength={8}/>
+                    <div className='profile_container_thre_input_eye' onClick={handleEyeClick}>👁️<p id='profile_container_three_input_eye_cross' style={{position:'relative', top:'-32px', left:'3px', fontSize:"24px", opacity:'1', marginTop:"7px"}}>/</p></div>
 
-                <label className='profile_container_three_label'>Mobile Number</label>
-                <input className='profile_container_three_input' type='text' readOnly value={mobileNumber}/><br></br>
+                    <label className='profile_container_three_label'>Mobile Number</label>
+                    <input className='profile_container_three_input' type='text' readOnly value={mobileNumber} id='profile_mobileNumber'/><br></br>
 
-                <label className='profile_container_three_label'>Date Of Borth</label>
-                <input className='profile_container_three_input' type='text' readOnly value={dateOfBirth}/><br></br>
+                    <label className='profile_container_three_label'>Date Of Borth</label>
+                    <input className='profile_container_three_input' type='text' readOnly value={dateOfBirth}/><br></br>
 
-                <label className='profile_container_three_label'>Gender</label>
-                <input className='profile_container_three_input' type='text' readOnly value={gender}/><br></br>
-            </div>
+                    <label className='profile_container_three_label'>Gender</label>
+                    <input className='profile_container_three_input' type='text' readOnly value={gender}/><br></br>
+
+                    <button className='profile_container_three_button_hidden' id='profile_container_three_button' onClick={handleSaveChanges} type='submit'>Save changes</button>
+                </div>
+            </form>
         </div>
     </div>
     )
